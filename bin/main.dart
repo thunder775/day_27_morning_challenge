7b8df2ac0db59402b8f17e0d7a8e0634b07e7839
thunder775 <rahulmasih61@gmail.com>
2019-11-20 14:19:35 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -19,6 +19,59 @@
 // two of which are objects with x & y values, and the third being the distance,
 // e.g. {x:50, y:60}, {x: 100, y: 100}, 10. The expected result is a similar
 // object with the new co-ordinate.
+import 'dart:convert';
+import 'dart:math';
+import 'dart:async';
+import 'package:executor/executor.dart';
+import 'package:http/http.dart';
 
-main() {
+List distance(Map point1, Map point2, int move1) {
+  double dis = pow(point1['x'] - point2['x'], 2).toDouble();
+  double dist = pow(point1['y'] - point2['y'], 2).toDouble();
+  double distance = sqrt(dis + dist);
+  print(distance);
+  double move2 = distance - move1;
+  print(move2);
+  print((point1['x'] * move2) + (point2['x'] * move1));
+  double x = ((point1['x'] * move2) + (point2['x'] * move1)) / (move1 + move2);
+  double y = ((point1['y'] * move2) + (point2['y'] * move1)) / (move1 + move2);
+  return [x, y];
+}
+
+Future averageWeight(int n)async {
+  List weights = [];
+  Executor executor = Executor(
+    concurrency: 4,
+  );
+
+  for (int i = 0; i < n; i++) {
+    executor.scheduleTask(() async {
+      var x = await getWeight(i);
+     if(x!=null){
+       weights.add(x);
+     };
+    });
+  }
+await executor.join(withWaiting: true);
+  return weights.reduce((a,b)=>a+b)/weights.length;
+}
+
+Future getWeight(int i) async {
+  print('request ${i + 1} made');
+  Response response =
+      await get('https://www.balldontlie.io/api/v1/players/${i + 1}');
+  Map map = jsonDecode(response.body);
+  print('request ${i + 1} completed and weight is ${map['weight_pounds']}');
+
+  return map['weight_pounds'];
+}
+
+main() async {
+  print(distance({'x': 50, 'y': 60}, {'x': 100, 'y': 100}, 10));
+
+//  var x = await getWeight(1);
+//  sleep(Duration(seconds: 3));
+//  print(x);
+  dynamic x = await averageWeight(5);
+  print(x);
 }
